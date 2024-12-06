5afacdb706e2fd453aa53b80e36ab23770d47536
Nazia Khan <naziaramzankhan@gmail.com>
2024-12-06 14:22:28 +0500

    Leetcode 501 solution added

---
+++ LeetCode501/LeetCode501/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCode501;
+
+Console.WriteLine("Hello, World!");
+Solution sol = new Solution();
+TreeNode root = new TreeNode(1);
+root.right = new TreeNode(2);
+root.right.right = new TreeNode(2);
+
+// Pass root to a function to perform an operation, e.g., inorder traversal
+sol.FindMode(root);
---
+++ LeetCode501/LeetCode501/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode501
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        public void InorderTraversal(TreeNode root)
+        {
+            if (root != null)
+            {
+                InorderTraversal(root.left);
+                if (dict.ContainsKey(root.val))
+                {
+                    dict[root.val]++;
+                }
+                else
+                {
+                    dict[root.val] = 1;
+                }
+                InorderTraversal(root.right);
+            }
+           
+        }
+        public int[] FindMode(TreeNode root)
+        {
+            InorderTraversal(root);
+            int maxFrequency = dict.Values.Max();
+
+            List<int> modes = dict.Where(x => x.Value == maxFrequency)
+                              .Select(x => x.Key).ToList();
+            return modes.ToArray();
+        }
+
+    }
+}
